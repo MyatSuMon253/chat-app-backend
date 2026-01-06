7edd4147d48d01b66c216636d9df5fe9c1378b30
MyatSuMon253 <myatsumon2531999@gmail.com>
2026-01-06 09:42:47 +0630

    feat: setup socket.io

--- socket/userEvents.ts
+++ socket/userEvents.ts
@@ -1,7 +1,7 @@
-import { Socket , Server as SocketIOServer } from "socket.io";
+import { Socket, Server as SocketIOServer } from "socket.io";
 
 export function registerUserEvents(io: SocketIOServer, socket: Socket) {
     socket.on('testSocket', (data: any) => {
-        socket.emit('testSocket', {message: `${data.name} says hello`})
+        socket.emit('testSocket', { message: `realtime update` })
     })
 }
